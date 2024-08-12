99f264292ce2f917714ac92f782dfecfb57b600a
Arthur Laroya <arthur.j.laroya@gmail.com>
2024-08-12 16:10:04 +1000

    next config images, cart modal, home page slider carousel

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,10 +1,88 @@
-"use client"
+"use client";
+
+import Image from "next/image";
 
 const CartModal = () => {
-  return (
-    // 43:19
-    <div>CartModal</div>
-  )
-}
+  const cartItems = true;
 
-export default CartModal
+  return (
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+      {!cartItems ? (
+        <div>Cart is empty</div>
+      ) : (
+        <>
+          <h2 className="text-lg">Shopping Cart</h2>
+          <div className="flex flex-col gap-8">
+            {/* item */}
+            <div className="flex gap-4">
+              <Image
+                src="https://placehold.co/600x400.png"
+                alt="cart"
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                <div>
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$50</div>
+                  </div>
+                  <div className="text-sm text-gray-500">available</div>
+                </div>
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
+
+            {/* item 2 */}
+            <div className="flex gap-4">
+              <Image
+                src="https://placehold.co/600x400.png"
+                alt="cart"
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                <div>
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$50</div>
+                  </div>
+                  <div className="text-sm text-gray-500">available</div>
+                </div>
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between font-semibold">
+              <span>Subtotal</span>
+              <span>$49</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4">
+              Shipping and taxes calculated at checkout.
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">
+                View Cart
+              </button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CartModal;
